615691eda56f318a33b1450b6553934a74a950ab
20vishwa <vishwateja157@gmail.com>
2017-06-14 11:34:54 +0530

    add fa icons

--- TIc TeC tOE/tictactoe.js
+++ TIc TeC tOE/tictactoe.js
@@ -1,17 +1,41 @@
 var change = true;
-$('#onePlayer').click(function () {
-    $('#firstPage').slideToggle();
-    $('#secPage').fadeToggle(3000);
-});
-$('#twoPlayer').click(function () {
-    $('#firstPage').toggle();
-    $('#secPage').fadeToggle(2000);
-});
-$('#secPage').on('click', '#back', function () {
-    $('#secPage').toggle();
-    $('#firstPage').fadeToggle(2000);
-});
-$('#thirdPage').on('click', '#back', function () {
-    $('#thirdPage').toggle();
-    $('#firstPage').fadeToggle(2000);
+var select;
+
+$(document).ready(function () {
+
+    $('#onePlayer').click(function () {
+        $('#firstPage').toggle();
+        $('#secPage').fadeToggle(2000);
+    });
+    $('#twoPlayer').click(function () {
+        $('#firstPage').toggle();
+        $('#thirdPage').fadeToggle(2000);
+    });
+    $('#secPage').on('click', '#back', function () {
+        $('#secPage').toggle();
+        $('#firstPage').fadeToggle(2000);
+    });
+    $('#thirdPage').on('click', '#back', function () {
+        $('#thirdPage').toggle();
+        $('#firstPage').fadeToggle(2000);
+    });
+    $('#thirdPage').on('click', '.ex', function () {
+        $('#thirdPage').toggle();
+        $('#gameBoard').fadeToggle();
+        select = "ex";
+    });
+    $('#thirdPage').on('click', '.oh', function () {
+        $('#thirdPage').toggle();
+        $('#gameBoard').fadeToggle();
+        select = "oh";
+    });
+    $('#gameBoard').on('click', '.square', function () {
+        if(select=="oh"){
+            $(this).addClass("fa fa-circle-o fa-4x");
+            select="ex";
+        }else{
+            $(this).addClass("fa fa-times fa-4x");
+            select="oh";
+        }
+    });
 });
